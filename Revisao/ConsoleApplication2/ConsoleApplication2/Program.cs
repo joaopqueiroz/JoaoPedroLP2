a5cb15b19706b34e19964a7f3b86b13c8d9a38fa
João Pedro Q Felippe <jpmaomessi@gmail.com>
2017-05-02 15:48:45 -0300

    responta exercicio 1

---
+++ Revisao/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string modelo, T, P;
+            double km, potencia;
+
+            Console.WriteLine("Informe o modelo do carro");
+            modelo = Console.ReadLine();
+
+            Console.WriteLine("Informe a quilometragem do carro");
+            km = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe a potencia do carro");
+            potencia = Convert.ToDouble(Console.ReadLine());
+
+            if (km < 5000)
+                T = "novo";
+            else if (km >= 5000 && km <= 30000 )
+                T = "seminovo";
+            else
+                T = "velho";
+            if (potencia > 200)
+                P = "potente";
+            else if (potencia > 120 && potencia <= 200)
+               P = "forte";
+            else
+                P = "popular";
+            
+            Console.WriteLine("O {0} - {1} - {2}", modelo, T, P);
+            Console.ReadKey();
+        }
+    }
+}
