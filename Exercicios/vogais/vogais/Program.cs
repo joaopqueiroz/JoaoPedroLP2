a725ace70c4ad51658a1ec1e014afff31eecb3d4
joaopqueiroz <jpmaomessi@gmail.com>
2017-05-24 15:52:47 -0300

    vogais e correçao

---
+++ Exercicios/vogais/vogais/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vogais;
+            int cont = 0;
+            vogais = Console.ReadLine();
+
+            for (int i = 0; i < vogais.Length; i++)
+            {
+                if (vogais[i] == 'a' || vogais[i] == 'A')
+                {
+                    cont++;
+                }
+
+                else if (vogais[i] == 'e' || vogais[i] == 'E')
+                {
+                    cont++;
+                }
+
+                else if (vogais[i] == 'i' || vogais[i] == 'I')
+                {
+                    cont++;
+                }
+
+                else if (vogais[i] == 'o' || vogais[i] == 'O')
+                {
+                    cont++;
+                }
+
+                else if (vogais[i] == 'u' || vogais[i] == 'U')
+                {
+                    cont++;
+                }
+
+            }
+            Console.WriteLine(cont);
+
+        }
+    }
+}
