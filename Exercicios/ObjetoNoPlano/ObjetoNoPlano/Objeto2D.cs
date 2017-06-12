98e94cc032b05e03be655073cb2e8ee839532fdc
joaopqueiroz <jpmaomessi@gmail.com>
2017-06-12 13:59:46 -0300

    resposta Objeto No Plano

---
+++ Exercicios/ObjetoNoPlano/ObjetoNoPlano/Objeto2D.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Objeto2D
+    {
+        public int X;
+        public int Y;
+
+        public void MoverParaDireita()
+        {
+            X += 1;
+        }
+
+        public string Coordenadas()
+        {
+            return String.Format("({0},{1})", X, Y);
+        }
+    }
+}
---
+++ Exercicios/ObjetoNoPlano/ObjetoNoPlano/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite X inicial: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite Y inicial: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Objeto2D obj = new Objeto2D();
+
+            obj.X = x;
+            obj.Y = y;
+
+            Console.Write("Digite comando: ");
+            ConsoleKey comando = Console.ReadKey().Key;
+
+
+            while (comando != ConsoleKey.Escape)
+            {
+                if (comando == ConsoleKey.RightArrow)
+                    obj.MoverParaDireita();
+
+                Console.WriteLine(obj.Coordenadas());
+
+                Console.Write("Digite comando: ");
+                comando = Console.ReadKey().Key;
+            }
+        }
+    }
+}
