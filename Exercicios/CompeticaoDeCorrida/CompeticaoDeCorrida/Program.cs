58c0dee4c402adc9581232e1878441b86059624a
joaopqueiroz <jpmaomessi@gmail.com>
2017-06-12 15:09:26 -0300

    Meu int.Parse e Competiçao de corrida

---
+++ Exercicios/CompeticaoDeCorrida/CompeticaoDeCorrida/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompeticaoDeCorrida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número de competidores");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Competidor[] comp = new Competidor[n];
+            double[] tempoOf = new double[n];
+
+            int i = 0;
+            while (i < n)
+            {
+                comp[i] = new Competidor();
+                
+                Console.WriteLine("Informe o nome");
+                comp[i].nome = Console.ReadLine();
+                
+                Console.WriteLine("Informe o primeiro tempo");
+                comp[i].t1 = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o segundo tempo");
+                comp[i].t2 = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o terceiro tempo");
+                comp[i].t3 = Double.Parse(Console.ReadLine());
+                
+                tempoOf[i] = comp[i].TempoOficial();
+
+                i++;
+            }
+        }
+    }
+}
---
+++ Exercicios/MeuIntParse/MeuIntParse/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuIntParse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2 * MeuIntParse(Console.ReadLine()));
+        }
+
+        public static int MeuIntParse(string num)
+        {
+            int multiplicar = 1;
+            int total = 0;
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                total += (num[i] - '0') * multiplicar;
+                multiplicar *= 10;
+            }
+
+            return total;
+        }
+    }
+
+}
