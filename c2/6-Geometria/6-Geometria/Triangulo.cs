0f4e9e9868b527f9b82b97520ec0f20085783834
JairoJuniorlp2 <jairojunior10410@gmail.com>
2017-09-20 13:38:11 -0300

    n terminado

---
+++ c2/6-Geometria/6-Geometria/Triangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Geometria
+{
+    class Triangulo
+    {
+        public double bas { get; set; }
+        public double altura { get; set; }
+
+        public double lado1 { get; set; }
+
+        public double lado2 { get; set; }
+
+        public double cosseno { get; set; }
+        
+        public double perimetro(double l1, double l2, double l3)
+        {
+            return l1 + 12 + 13;
+        }
+
+        public double area(double b, double h)
+        {
+             return (b * h) / 2;
+        }
+
+    }
+}
