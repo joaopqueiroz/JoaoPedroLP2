35056d9b2d4af465fd32e03f8645096748449aff
JairoJuniorlp2 <jairojunior10410@gmail.com>
2017-09-20 13:38:41 -0300

    conversor

---
+++ c2/7-Conversor/7-Conversor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempC t = new TempC();
+            int n = 1;
+
+            while (n != 0)
+            {
+                Console.WriteLine("1- Nova temperatura");
+                Console.WriteLine("2- Para Fahrenheit");
+                Console.WriteLine("3- Para Kelvin");
+                Console.WriteLine("0- Sair");
+                n = int.Parse(Console.ReadLine());
+
+                if (n != 0)
+                {
+                    Console.WriteLine("Temperatura:");
+                    t.valor = double.Parse(Console.ReadLine());
+
+                    if (n == 2)
+                        Console.WriteLine(t.EmFahrenheit());
+                    else if (n == 3)
+                        Console.WriteLine(t.EmKelvin());
+                }
+                else
+                    break;
+            }
+        }
+    }
+}
+       
---
+++ c2/7-Conversor/7-Conversor/TempC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Conversor
+{
+    class TempC
+    {
+        public double valor;
+
+        public double EmFahrenheit()
+        {
+            return 1.8 * valor + 32;
+        }
+
+        public double EmKelvin()
+        {
+            return valor + 273;
+        }
+    }
+}
