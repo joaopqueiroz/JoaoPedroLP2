b2233bbb479ce3b3b7450c9b6949fdb048fbca59
JairoJuniorlp2 <jairojunior10410@gmail.com>
2017-09-20 13:27:03 -0300

    trab produto

---
+++ c2/5-Produtos/5-Produtos/Produto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Produtos
+{
+    class Produto
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+        }
+
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        private int quantidade;
+        public int Qquantidade
+        {
+            get { return quantidade; }
+        }
+
+        public string Marca { get; set; }
+        public double Preço { get; set; }
+
+        public void Repor(int qtd)
+        {
+            quantidade += qtd;
+        }
+
+        public void Retirada(int qtd)
+        {
+            if (quantidade >= qtd)
+                quantidade -= qtd;
+
+            else throw new Exception("Uma retirada não pode exceder a quantidade em estoque atual");
+        }
+
+        public string Imprimir()
+        {
+            string x = Preço.ToString("0.00");
+            return String.Format("Produto {0} : {1} - R$ {2} - Estoque : {3}", id, nome, x, quantidade);
+        }
+
+        public Produto(int id, string nome, double preço)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.Preço = preço;
+            this.quantidade = 0;
+        }
+    }
+}
---
+++ c2/5-Produtos/5-Produtos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Informe os dados do Produto");
+
+            Console.WriteLine("Id: ");
+            int id = int.Parse(Console.ReadLine());
+                
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+                
+            Console.WriteLine("Preço: ");
+            double preço = double.Parse(Console.ReadLine());
+
+            Produto prod = new Produto(id, nome, preço);
+            
+            int i = 0;
+            while (i != 4)
+            {
+                Console.WriteLine("1 - Repor");
+                Console.WriteLine("2 - Retirar");
+                Console.WriteLine("3 - Imprimir");
+                Console.WriteLine("4 - Sair");
+
+                i = int.Parse(Console.ReadLine());
+
+                if (i == 1)
+                {
+                    Console.WriteLine("Quantidade ganha: ");
+                    int x = int.Parse(Console.ReadLine());
+                    prod.Repor(x);
+                }
+
+                if (i == 2)
+                {
+                    Console.WriteLine("Quantidade retirada: ");
+                    int x = int.Parse(Console.ReadLine());
+                    prod.Retirada(x);
+                }
+
+                if (i == 3)
+                {
+                    Console.WriteLine(prod.Imprimir());
+                }
+            }
+        }
+    }
+}
